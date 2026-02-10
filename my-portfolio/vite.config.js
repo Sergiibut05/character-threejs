97bd33df2e77f8eb6d1b8d2b543ca182daea3eed
Sergii Butrii <sergiibutrii555@gmail.com>
2026-02-10 09:23:30 +0100

    Refactor Vite configuration for clarity and structure

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -3,23 +3,23 @@ import wasm from 'vite-plugin-wasm'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
 export default {
-    root: 'src/', // Sources files (typically where index.html is)
-    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
-    server:
-    {
-        host: true, // Open to local network and display URL
-        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env) // Open if it's not a CodeSandbox
+    root: 'src/',
+    publicDir: '../static/',
+    server: {
+        host: true,
+        open: !('SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env)
     },
-    build:
-    {
-        outDir: '../dist', // Output in the dist/ folder
-        emptyOutDir: true, // Empty the folder first
-        sourcemap: true // Add sourcemap
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        sourcemap: true,
+
+        
+        target: 'esnext'
     },
-    plugins:
-    [
-        basicSsl(), // Enable HTTPS with compatible self-signed certificate
-        wasm(), // Enable WASM support for Rapier.js
-        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    plugins: [
+        basicSsl(),
+        wasm(),
+        restart({ restart: ['../static/**'] })
     ],
 }
